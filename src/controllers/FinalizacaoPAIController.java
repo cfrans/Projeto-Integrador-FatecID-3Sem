64724fe2d710d3446581ab190c6b3e207ca74dc6
Vitoria Faturi <cvitoriak@gmail.com>
2025-09-08 19:43:26 -0300

    Ajuste opções menu e outras telas

--- src/controllers/FinalizacaoPAIController.java
+++ src/controllers/FinalizacaoPAIController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class PleaseProvideControllerClassName {
+public class FinalizacaoPAIController {
 
     @FXML
     private Button btImprimir;
