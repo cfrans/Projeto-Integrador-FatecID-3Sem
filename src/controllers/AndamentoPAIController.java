b9ffbeebc068d0db16e26456b4e36cbeed41939e
Vitoria Faturi <cvitoriak@gmail.com>
2025-10-13 18:49:31 -0300

    Vinculaçao de Telas

--- src/controllers/AndamentoPAIController.java
+++ src/controllers/AndamentoPAIController.java
@@ -2,9 +2,16 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AndamentoPAIController {
 
@@ -29,14 +36,23 @@ public class AndamentoPAIController {
     @FXML
     private TextField tfTituloPlano;
 
-    @FXML
-    void onClickAbrirDescricao(ActionEvent event) {
-
-    }
 
     @FXML
-    void onClickCriarFinalizacao(ActionEvent event) {
-
+    void onClickAbrirDescricao(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/PAI.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
+
+    @FXML
+    void onClickCriarFinalizacao(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/FinalizacaoPAI.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/controllers/FinalizacaoPAIController.java
+++ src/controllers/FinalizacaoPAIController.java
@@ -1,12 +1,22 @@
+package controllers;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FinalizacaoPAIController {
+
 
     @FXML
     private Button btImprimir;
@@ -81,7 +91,18 @@ public class FinalizacaoPAIController {
 
     @FXML
     void onClickVoltar(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AndamentoPAI.fxml"));
+            Parent root = loader.load();
 
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
